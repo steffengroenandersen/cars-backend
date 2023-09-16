df6f36811e8b7f0705741192b2410b59c3257423
steffengroenandersen <steffengroenandersen@gmail.com>
2023-09-16 20:49:49 +0200

    Update CarService

--- src/main/java/sem3/cars/api/CarController.java
+++ src/main/java/sem3/cars/api/CarController.java
@@ -7,7 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/cars")
 @RestController
 public class CarController {
-    CarService carService;
     
-    public CarController
 }
