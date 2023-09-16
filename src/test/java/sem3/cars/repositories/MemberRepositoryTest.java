6e19a1bd211f32df005ccece9ca6686e1d625083
steffengroenandersen <steffengroenandersen@gmail.com>
2023-09-16 17:59:04 +0200

    Add MemberRepositoryTest

--- src/test/java/sem3/cars/repositories/MemberRepositoryTest.java
+++ src/test/java/sem3/cars/repositories/MemberRepositoryTest.java
@@ -1,18 +1,30 @@
 package sem3.cars.repositories;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import sem3.cars.entity.Member;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Hello
- * 
- */
-
+@DataJpaTest
 class MemberRepositoryTest {
 
-    //TODO Create unit tests
+    @Autowired
+    MemberRepository memberRepository;
+    
     @BeforeEach
     void setUp() {
+        Member m1 = new Member("member1", "test123", "member1@testmail.com", "Anders", "Holm", "astreet", "acity", "2000");
+        memberRepository.save(m1);
+    }
+    @Test
+    void findMembersByZip() {
+        List<Member> members = memberRepository.findMembersByZip("2000");
+        assertEquals(1, members.size());
+        assertEquals("2000", members.get(0).getZip());
     }
 }
